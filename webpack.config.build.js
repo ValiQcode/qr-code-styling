2793fab2b8a3d9764b71b6e3c4554687332b2bf7
ValiQ code <bquast@valiq.com>
2024-09-28 16:40:29 +0200

    Update webpack.config.build.js

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,9 +1,17 @@
+const path = require('path');
 const commonConfig = require('./webpack.config.common.js');
 const config = commonConfig;
 
 module.exports = (env, argv) => {
   config.mode = argv.mode;
 
+  // Set the output directory to 'public' for Vercel
+  config.output = {
+    path: path.resolve(__dirname, 'public'), // Output files to 'public'
+    filename: 'bundle.js' // The name of the output bundle file
+  };
+
+  // Source maps for development or production
   if (argv.mode === "development") {
     config.devtool = "inline-source-map";
     config.watch = true;
