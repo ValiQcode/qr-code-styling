89737166b7a8815bece907467404b3137d560ccb
ValiQ code <bquast@valiq.com>
2024-09-28 18:46:52 +0200

    Update webpack.config.build.js

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,33 +1,21 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const commonConfig = require('./webpack.config.common.js');
-const config = commonConfig;
 
 module.exports = (env, argv) => {
-  config.mode = argv.mode;
-
-  // Set the output directory to 'public' for Vercel
-  config.output = {
-    path: path.resolve(__dirname, 'public'), // Ensure output is in 'public'
-    filename: 'bundle.js', // The output JavaScript bundle
+  return {
+    mode: argv.mode,
+    entry: './src/index.ts',
+    output: {
+      path: path.resolve(__dirname, 'public'), // Ensure output is in 'public'
+      filename: 'bundle.js', // Output JavaScript bundle
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html', // Ensure index.html is outputted to 'public'
+        filename: 'index.html'
+      }),
+      // Remove FileManagerPlugin if it's unnecessary or reconfigure to copy to 'public'
+    ],
+    // Add necessary loaders or other configurations
   };
-
-  // Use HtmlWebpackPlugin to move index.html to 'public'
-  config.plugins = [
-    new HtmlWebpackPlugin({
-      template: './src/index.html', // The source index.html file in 'src'
-      filename: 'index.html' // Output to 'public/index.html'
-    }),
-  ];
-
-  if (argv.mode === 'development') {
-    config.devtool = 'inline-source-map';
-    config.watch = true;
-  }
-
-  if (argv.mode === 'production') {
-    config.devtool = 'source-map';
-  }
-
-  return config;
 };
