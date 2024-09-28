1a8fa6e7e6fb62381681a014bd9f4c35ad07c546
ValiQ code <bquast@valiq.com>
2024-09-28 18:12:13 +0200

    Update webpack.config.build.js add public folder configuration

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const commonConfig = require('./webpack.config.common.js');
 const config = commonConfig;
 
@@ -8,17 +9,24 @@ module.exports = (env, argv) => {
   // Set the output directory to 'public' for Vercel
   config.output = {
     path: path.resolve(__dirname, 'public'), // Output files to 'public'
-    filename: 'bundle.js' // The name of the output bundle file
+    filename: 'bundle.js', // The name of the output bundle file
   };
 
-  // Source maps for development or production
-  if (argv.mode === "development") {
-    config.devtool = "inline-source-map";
+  // Add HtmlWebpackPlugin to automatically move index.html to 'public'
+  config.plugins = [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // The source index.html file
+      filename: 'index.html' // The output file in 'public' directory
+    })
+  ];
+
+  if (argv.mode === 'development') {
+    config.devtool = 'inline-source-map';
     config.watch = true;
   }
 
-  if (argv.mode === "production") {
-    config.devtool = "source-map";
+  if (argv.mode === 'production') {
+    config.devtool = 'source-map';
   }
 
   return config;
